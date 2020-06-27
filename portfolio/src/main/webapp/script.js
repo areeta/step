eb972b906be8e04c875ce601c9a5987385c55913
areetaw <areetaw@gmail.com>
2020-06-27 15:07:45 -0700

    add newlines to end of files and change myFunction to handleOnScroll

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -28,9 +28,9 @@ function addRandomFact() {
 }
 
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {handleOnScroll()};
 
-function myFunction() {
+function handleOnScroll() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
