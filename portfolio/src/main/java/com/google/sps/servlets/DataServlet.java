db01856eabee8bd6ff789a725d449935e83e7c04
areetaw <areetaw@gmail.com>
2020-06-30 18:38:13 +0000

    order imports, add newline to script.js, and end comment with period

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,15 +14,15 @@
 
 package com.google.sps.servlets;
 
-import java.io.IOException;
 import com.google.gson.Gson;
+import java.io.IOException;
 import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle messages data */
+/** Servlet that returns some example content. */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   
@@ -31,7 +31,7 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    // Convert the messages to JSON
+    // Convert the messages to JSON.
     messages = new ArrayList<>();
     messages.add("boop bop beep");
     messages.add("bopity beep boop");
